74df915add7cd3ac65d8b6ffa8926693a43f68b4
filipejml <brincandodedesign@gmail.com>
2026-07-08 09:46:47 -0300

    refactor: logica dos seeders e adaptação

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,18 +10,22 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::create([
-            'name' => 'Administrador',
-            'email' => 'admin@fantasyvolei.com',
-            'password' => Hash::make('12345678'),
-            'role' => 0,
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@fantasyvolei.com'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('12345678'),
+                'role' => 0,
+            ]
+        );
 
-        User::create([
-            'name' => 'Usuário Teste',
-            'email' => 'usuario@fantasyvolei.com',
-            'password' => Hash::make('12345678'),
-            'role' => 1,
-        ]);
+        User::updateOrCreate(
+            ['email' => 'usuario@fantasyvolei.com'],
+            [
+                'name' => 'Usuario Teste',
+                'password' => Hash::make('12345678'),
+                'role' => 1,
+            ]
+        );
     }
 }
